63f384a37532e1db94bac72dc4c5832785ca383e
Roman Protsyk <rprotsyk@gmail.com>
2022-12-14 15:43:37 +0200

    redux toolkit

--- apps/frontend/src/store/features/session/session-slice.action.ts
+++ apps/frontend/src/store/features/session/session-slice.action.ts
@@ -1,17 +1,3 @@
-/*import {createAsyncThunk} from '@reduxjs/toolkit';
-import { LoginDto } from '@boilerplate/shared';
-import { usersService } from '../../../data-services';
-
-//
-const loginUserAction = createAsyncThunk('user/login', async (body: LoginDto, { rejectWithValue }) => {
-  try {
-    const { data } = await usersService.login(body);
-    return data;
-  } catch (error) {
-    return rejectWithValue((error as Error).message);
-  }
-})
-*/
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import { LoginDto } from '@boilerplate/shared';
 import { usersService } from '../../../data-services';
