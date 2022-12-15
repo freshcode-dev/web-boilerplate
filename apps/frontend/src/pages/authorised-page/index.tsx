f8661688c3930eaddf89bd85f600d13c1312b194
Roman Protsyk <rprotsyk@gmail.com>
2022-12-15 14:00:41 +0200

    get users

--- apps/frontend/src/pages/authorised-page/index.tsx
+++ apps/frontend/src/pages/authorised-page/index.tsx
@@ -2,17 +2,21 @@ import React, { FC } from 'react';
 import { CoreButton, CoreCheckbox, CoreTextField, CoreSelect } from '../../components/_ui';
 import { logOut } from '../../store/features/session';
 import { useAppDispatch } from '../../store';
+import { useGetUsersQuery } from '../../store/features/users'
 
 const AuthorizedPage: FC = () => {
   const dispatch = useAppDispatch();
   const handleLogoutClick = async () => {
     dispatch(logOut())
   }
-
+  // Using a query hook automatically fetches data and returns query values
+  const { data, error, isLoading } = useGetUsersQuery('');
   return (
     <div>
       authorized page content
-      <br/>
+      <br />
+      {JSON.stringify(data)}
+      <br />
       <CoreButton onClick={handleLogoutClick}>Logout</CoreButton>
       <CoreCheckbox></CoreCheckbox>
       <CoreSelect
@@ -23,7 +27,7 @@ const AuthorizedPage: FC = () => {
         }}
       >
       </CoreSelect>
-      
+
       <CoreTextField size="small"></CoreTextField>
     </div>
   )
